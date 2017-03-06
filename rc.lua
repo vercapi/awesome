6b9d4f3afe25b72fa2084f6cfd2030a67b9be986
Pieter <pieterv.sorano@gmail.com>
2017-03-06 11:42:43 +0100

    Added a tiling layout for all windows

--- rc.lua
+++ rc.lua
@@ -266,7 +266,7 @@
          set_wallpaper(s)  
      
          -- Each screen has its own tag table.  
-         awful.tag({ "x01", "x02", "x03", "x04", "x05", "x06", "x07", "x08", "x09"}, s, awful.layout.layouts[1])  
+         awful.tag({ "x01", "x02", "x03", "x04", "x05", "x06", "x07", "x08", "x09"}, s, awful.layout.layouts[2])  
      
          -- Create a promptbox for each screen  
          s.mypromptbox = awful.widget.prompt()  
